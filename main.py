ad0b68bc1b6d3724a9c89bc3add51b48bad513a0
tobi <tobibags19@gmail.com>
2023-05-08 06:05:41 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+# Create a canvas Instance
+myturtle = turtle.Turtle()
+
+# Go to a certain coordinate
+myturtle.penup()
+myturtle.goto(50, 75)
+
+
+myturtle.pendown()
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
